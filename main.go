387ec4af1a5ccfa7e88c728a4d3b700018a82e81
obigvee <adedejivictor98@gmail.com>
2024-05-28 03:24:10 +0100

    :tada:

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/anthdm/hollywood/actor"
+)
 
 func main() {
     fmt.Println("Ultron! Do you read the world?")
+
+    e, err := actor.NewEngine(actor.NewEngineConfig())
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    e.SpawnFunc(func (c *actor.Context)  {
+        switch msg := c.Message().(type){
+        case actor.Started:
+            fmt.Println("started")
+            _ = msg
+        }
+    }, "foo")
+        time.Sleep(time.Second * 10)
 }
